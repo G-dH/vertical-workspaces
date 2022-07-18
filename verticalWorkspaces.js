b807f9a77097a1d05e07f5d3c28f2c6e9a706770
G-dH <georgdh@gmail.com>
2022-07-19 00:24:21 +0200

    fix: static app grid stays visible under workspaces view

--- verticalWorkspaces.js
+++ verticalWorkspaces.js
@@ -1132,10 +1132,12 @@ var ControlsManagerOverride = {
             mode: Clutter.AnimationMode.EASE_OUT_QUART,//EASE_OUT_QUAD,
         }
 
+        this._appDisplay.opacity = 255 - opacity;
         // workspacesDisplay needs to go off screen, otherwise it blocks DND operations within the App Display
         // but the 'visibile' property ruins transition animation and breakes workspace control
         // scale_y = 0 works like visibile = false but without collateral damage
         this._workspacesDisplay.scale_y = (progress == 1 && finalState == ControlsState.APP_GRID) ? 0 : 1;
+        this._appDisplay.scale_y = (progress == 1 && finalState != ControlsState.APP_GRID) ? 0 : 1;
         this._workspacesDisplay.reactive = workspacesDisplayVisible;
         this._workspacesDisplay.setPrimaryWorkspaceVisible(workspacesDisplayVisible);
         // scale workspaces back to normal size before transition from AppGrid view
