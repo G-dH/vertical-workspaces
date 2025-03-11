G-dH/vertical-workspaces

commit dbdfc35514cc8bc9207575a245fcad543fbc4e3e

workspace, workspacesView, ...: Fix Tab key navigation between windows in the overview

lib/workspace.js
@@ -3,15 +3,17 @@
  * workspace.js
  *
  * @author     GdH <G-dH@github.com>
- * @copyright  2022 - 2024
+ * @copyright  2022 - 2025
  * @license    GPL-3.0
  *
  */
 
 'use strict';
 
+import Clutter from 'gi://Clutter';
+import GObject from 'gi://GObject';
 import St from 'gi://St';
-// import Graphene from 'gi://Graphene';
+import Graphene from 'gi://Graphene';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as Workspace from 'resource:///org/gnome/shell/ui/workspace.js';
@@ -63,6 +65,10 @@ export const WorkspaceModule = class {
 
         // fix overlay base for Vertical Workspaces
         this._overrides.addOverride('WorkspaceLayout', Workspace.WorkspaceLayout.prototype, WorkspaceLayout);
+
+        // Add support for Tab key navigation in the overview
+        this._overrides.addOverride('Workspace', Workspace.Workspace.prototype, WorkspaceCommon);
+
         console.debug('  WorkspaceModule - Activated');
     }
 
@@ -76,6 +82,82 @@ export const WorkspaceModule = class {
     setWindowPreviewMaxScale(scale) {
         WINDOW_PREVIEW_MAXIMUM_SCALE = scale;
     }
+};
+
+const WorkspaceCommon = {
+    _init(metaWorkspace, monitorIndex, overviewAdjustment) {
+        St.Widget.prototype._init.bind(this)({
+            style_class: 'window-picker',
+            pivot_point: new Graphene.Point({ x: 0.5, y: 0.5 }),
+            layout_manager: new Clutter.BinLayout(),
+        });
+
+        const layoutManager = new Workspace.WorkspaceLayout(metaWorkspace, monitorIndex,
+            overviewAdjustment);
+
+        // Background
+        this._background =
+            new Workspace.WorkspaceBackground(monitorIndex, layoutManager.stateAdjustment);
+        this.add_child(this._background);
+
+        // Window previews
+        // Replace the default Actor container with St.Widget
+        // to allow Tab navigation between window previews
+        // Official GNOME Shell bug report https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/5345
+        // Implemented merge request https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2591
+        this._container = new WorkspaceWindowContainer({ layoutManager });
+        this.add_child(this._container);
+        global.focus_manager.add_group(this._container);
+
+        this.metaWorkspace = metaWorkspace;
+
+        this._overviewAdjustment = overviewAdjustment;
+
+        this.monitorIndex = monitorIndex;
+        this._monitor = Main.layoutManager.monitors[this.monitorIndex];
+
+        if (monitorIndex !== Main.layoutManager.primaryIndex)
+            this.add_style_class_name('external-monitor');
+
+        const clickAction = new Clutter.ClickAction();
+        clickAction.connect('clicked', action => {
+            // Switch to the workspace when not the active one, leave the
+            // overview otherwise.
+            if (action.get_button() === 1 || action.get_button() === 0) {
+                const leaveOverview = this._shouldLeaveOverview();
+
+                this.metaWorkspace?.activate(global.get_current_time());
+                if (leaveOverview)
+                    Main.overview.hide();
+            }
+        });
+        this.bind_property('mapped', clickAction, 'enabled', GObject.BindingFlags.SYNC_CREATE);
+        this._container.add_action(clickAction);
+
+        this.connect('style-changed', this._onStyleChanged.bind(this));
+        this.connect('destroy', this._onDestroy.bind(this));
+
+        this._skipTaskbarSignals = new Map();
+        this._windows = [];
+        this._layoutFrozenId = 0;
+
+        // DND requires this to be set
+        this._delegate = this;
+
+        // Track window changes, but let the window tracker process them first
+        this.metaWorkspace?.connectObject(
+            'window-added', this._windowAdded.bind(this), GObject.ConnectFlags.AFTER,
+            'window-removed', this._windowRemoved.bind(this), GObject.ConnectFlags.AFTER,
+            'notify::active', () => layoutManager.syncOverlays(), this);
+        global.display.connectObject(
+            'window-entered-monitor', this._windowEnteredMonitor.bind(this), GObject.ConnectFlags.AFTER,
+            'window-left-monitor', this._windowLeftMonitor.bind(this), GObject.ConnectFlags.AFTER,
+            this);
+
+        // Create clones for windows that should be
+        // visible in the Overview
+        global.get_window_actors().map(a => this._doAddWindow(a.meta_window));
+    },
 };
 
 // Workaround for an upstream bug affecting window scaling and positioning:
@@ -476,3 +558,12 @@ const WorkspaceBackground = {
             Util.lerp(0, cornerRadius, value);
     },
 };
+
+const WorkspaceWindowContainer = GObject.registerClass({
+    // Registered name should be unique
+    GTypeName: `FolderGrid${Math.floor(Math.random() * 1000)}`,
+}, class WorkspaceWindowContainer extends St.Widget {
+    vfunc_get_focus_chain() {
+        return this.layout_manager.getFocusChain();
+    }
+});

lib/util.js
@@ -3,7 +3,7 @@
  * util.js
  *
  * @author     GdH <G-dH@github.com>
- * @copyright  2022 - 2024
+ * @copyright  2022 - 2025
  * @license    GPL-3.0
  *
  */
@@ -243,6 +243,19 @@ export function isAltPressed(state = null) {
     if (state === null)
         [,, state] = global.get_pointer();
     return (state & Clutter.ModifierType.MOD1_MASK) !== 0;
+}
+
+export function isSuperPressed(state = null) {
+    if (state === null)
+        [,, state] = global.get_pointer();
+    return (state & Clutter.ModifierType.MOD4_MASK) !== 0;
+}
+
+export function focusDash() {
+    Main.ctrlAltTabManager._items.forEach(i => {
+        if (i.sortGroup === 1 && i.name === 'Dash')
+            Main.ctrlAltTabManager.focusGroup(i);
+    });
 }
 
 export function fuzzyMatch(term, text) {

lib/windowManager.js
@@ -3,7 +3,7 @@
  * windowManager.js
  *
  * @author     GdH <G-dH@github.com>
- * @copyright  2022 - 2024
+ * @copyright  2022 - 2025
  * @license    GPL-3.0
  *
  */
@@ -117,10 +117,14 @@ const WindowManagerCommon = {
         if (!Main.sessionMode.hasWorkspaces)
             return;
 
-        if (opt.WS_SWITCHER_CURRENT_MONITOR)
+        if (opt.WS_SWITCHER_CURRENT_MONITOR) {
             this._switchWorkspaceCurrentMonitor(workspace);
-        else if (!workspace.active)
+        } else if (!workspace.active) {
             workspace.activate(global.get_current_time());
+            // Remove focus from the previous workspace view
+            // Part of the merge request https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2591
+            global.stage.set_key_focus(null);
+        }
     },
 
     actionMoveWindow(window, workspace) {

lib/workspacesView.js
@@ -99,6 +99,17 @@ export const WorkspacesViewModule = class {
 };
 
 const WorkspacesViewCommon = {
+    // Allow Tab navigation between window previews
+    // GNOME Shell upstream bug report https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/5345
+    // Following two methods are part of the merge request https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2591
+    after__init() {
+        global.focus_manager.remove_group(this);
+    },
+
+    vfunc_navigate_focus(from, direction) {
+        return this.getActiveWorkspace().navigate_focus(from, direction, false);
+    },
+
     _getFirstFitSingleWorkspaceBox(box, spacing, vertical) {
         let [width, height] = box.get_size();
         const [workspace] = this._workspaces;
@@ -910,18 +921,15 @@ const WorkspacesDisplayCommon = {
             which = workspaceManager.n_workspaces - 1;
             break;
         case Clutter.KEY_space:
-            if (Me.Util.isCtrlPressed() && Me.Util.isShiftPressed()) {
+            if (Me.Util.isCtrlPressed() && Me.Util.isShiftPressed())
                 Me.Util.openPreferences();
-            } else if (Me.Util.isAltPressed()) {
-                Main.ctrlAltTabManager._items.forEach(i => {
-                    if (i.sortGroup === 1 && i.name === 'Dash')
-                        Main.ctrlAltTabManager.focusGroup(i);
-                });
-            } else if (Me.Util.getEnabledExtensions('extensions-search-provider').length && Me.Util.isCtrlPressed()) {
+            else if (Me.Util.isAltPressed())
+                Me.Util.focusDash();
+            else if (Me.Util.getEnabledExtensions('extensions-search-provider').length && Me.Util.isCtrlPressed())
                 Me.Util.activateSearchProvider(Me.ESP_PREFIX);
-            } else if (Me.Util.getEnabledExtensions('windows-search-provider').length) {
+            else if (Me.Util.getEnabledExtensions('windows-search-provider').length)
                 Me.Util.activateSearchProvider(Me.WSP_PREFIX);
-            }
+
 
             return Clutter.EVENT_STOP;
         case Clutter.KEY_Down:
@@ -929,6 +937,10 @@ const WorkspacesDisplayCommon = {
         case Clutter.KEY_Right:
         case Clutter.KEY_Up:
         case Clutter.KEY_Tab:
+            if (symbol === Clutter.KEY_Tab && Me.Util.isSuperPressed(event.get_state())) {
+                Me.Util.focusDash();
+                return Clutter.EVENT_STOP;
+            }
             if (Main.overview.searchController.searchActive) {
                 Main.overview.searchEntry.grab_key_focus();
             } else if (!opt.WORKSPACE_MODE && state <= 1) {

commit 47a5443d8ad77a21b23c0378d84b5628b30b6da3

workspace: Fix workspace preview ignores mouse clicks (caused by commit dbdfc35)

lib/workspace.js
@@ -105,7 +105,7 @@ const WorkspaceCommon = {
         // to allow Tab navigation between window previews
         // Official GNOME Shell bug report https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/5345
         // Implemented merge request https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2591
-        this._container = new WorkspaceWindowContainer({ layoutManager });
+        this._container = new WorkspaceWindowContainer({ layoutManager, reactive: true });
         this.add_child(this._container);
         global.focus_manager.add_group(this._container);
 